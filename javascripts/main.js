1c83148191f9506355722f063df547e240254323
ellisthomas <ellisthomas80@gmail.com>
2017-05-09 19:48:41 -0500

    first step in angular

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
+var app = angular.module("TodoApp", []);
 
+app.controller("NavCtrl", ($scope) => {
+	
+	$scope.cat = "Meow";
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof!";
+});
