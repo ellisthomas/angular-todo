9369e98372e2dcc96f458f21568f82a044ebe908
ellisthomas <ellisthomas80@gmail.com>
2017-05-13 11:43:28 -0500

    hooked up to firebase returning data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,61 @@
-
-app.controller("NavCtrl", ($scope) => {
-	
-	$scope.cat = "Meow";
-	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+app.run((FIREBASE_CONFIG) => {
+    firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("ItemCtrl", ($scope) => {
-	$scope.dog = "Woof!";
-	$scope.showListView = true;
-	$scope.items = [];
+app.controller("NavCtrl", ($scope) => {
 
-	$scope.newItem = () => {
-		$scope.showListView = false;
-	};
+    $scope.cat = "Meow";
+    $scope.navItems = [{
+        name: "Logout"
+    }, {
+        name: "All Items"
+    }, {
+        name: "New Item"
+    }];
+});
 
-	$scope.allItems = () => {
-		$scope.showListView = true;
-	};
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+    $scope.dog = "Woof!";
+    $scope.showListView = true;
+    $scope.items = [];
+
+    $scope.newItem = () => {
+        $scope.showListView = false;
+    };
+
+    $scope.allItems = () => {
+        $scope.showListView = true;
+    };
+
+    let getItemList = () => {
+        let itemz = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+                .then((fbItems) => {
+                    let itemCollection = fbItems.data;
+                    Object.keys(itemCollection).forEach((key) => {
+                        itemCollection[key].id = key;
+                        itemz.push(itemCollection[key]);
+                    });
+                    resolve(itemz);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
+
+    let getItems = () => {
+        getItemList().then((itemz) => {
+            $scope.items = itemz;
+        }).catch((error) => {
+            console.log("get Error", error);
+        });
+    };
+
+    getItems();
+
+
+
+
 });
