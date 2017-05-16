e719dc260a6b200e4428619de785a0ec6e17810c
ellisthomas <ellisthomas80@gmail.com>
2017-05-15 20:12:14 -0500

    working with partials

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,20 +1,48 @@
-app.run((FIREBASE_CONFIG) => {
+app.run(function(FIREBASE_CONFIG) {
     firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("NavCtrl", ($scope) => {
-
-    $scope.cat = "Meow";
-    $scope.navItems = [{
-        name: "Logout"
-    }, {
-        name: "All Items"
-    }, {
-        name: "New Item"
-    }];
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/items/list", {
+            templateUrl: "partials/item-list.html",
+            controller: "ItemListCtrl"
+        })
+        .when("/items/new", {
+            templateUrl: "partials/item-new.html",
+            controller: "ItemNewCtrl"
+        })
+        .when("item/view/:id", {
+            templateUrl: "partials/item-view.html",
+            controller: "ItemViewCtrl"
+        })
+        .when("/item/edit/:id", {
+            templateUrl: "partials/item-new.html",
+            controller: "ItemEditCtrl"
+        })
+        .otherwise("/items/list");
 });
 
-app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+app.controller("NavCtrl", function($scope) {
+
+    $scope.cat = "Meow";
+    $scope.navItems = [{ name: "Logout"}, { name: "All Items"}, { name: "New Item"}];
+});
+
+app.controller("ItemListCtrl", function() {
+    console.log("inside ItemListCtrl");
+});
+app.controller("ItemNewCtrl", function() {
+    console.log("inside ItemNewCtrl");
+});
+app.controller("ItemViewCtrl", function() {
+    console.log("inside ItemViewCtrl");
+});
+app.controller("ItemNewCtrl", function() {
+    console.log("inside ItemEditCtrl");
+});
+
+app.controller("ItemCtrl", function($http, $q, $scope, FIREBASE_CONFIG) {
     $scope.dog = "Woof!";
     $scope.showListView = true;
     $scope.items = [];
