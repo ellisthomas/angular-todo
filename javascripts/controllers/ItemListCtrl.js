650986689dc2259195fdf9f01ff506e64af32a23
ellisthomas <ellisthomas80@gmail.com>
2017-05-16 20:34:28 -0500

    checkboxes

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -19,4 +19,12 @@ app.controller("ItemListCtrl", function($scope, ItemFactory) {
             console.log("deleteItem error", error);
         });
     };
+
+    $scope.inputChange = (item) => {
+    	ItemFactory.editItem(item).then(() => {
+    		///????????
+    	}).catch((error) => {
+    		console.log("inputChange error", error);
+    	});
+    };
 });
